570232348237cc7adcfe28bbc6e5c61ebb27f18f
Arhaze Shally <arhazee90@yandex.ru>
2026-05-28 14:00:06 +0300

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Rubik } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const rubik = Rubik({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
