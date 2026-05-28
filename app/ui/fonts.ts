51787a6d5425f17b1cd287c3f509c121b192b3c3
Arhaze Shally <arhazee90@yandex.ru>
2026-05-28 15:30:30 +0300

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,15 @@
-import { Inter, Rubik } from 'next/font/google';
+import { Inter, Rubik, Lusitana } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
 });
 
 export const rubik = Rubik({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
